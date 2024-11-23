c6649eec6995e87838e20e628df647c011c59d02
GorbunovKirill <4pduck@gmail.com>
2024-11-23 21:14:42 +0300

    New commit

--- main.cpp
+++ main.cpp
@@ -2,10 +2,24 @@
 
 #include <QApplication>
 
+#include <QMainWindow>
+#include <QFile>
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QFile styleFile(":/MaterialDark.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString styleSheet = QLatin1String(styleFile.readAll());
+        a.setStyleSheet(styleSheet);
+        styleFile.close();
+    } else {
+        qDebug() << "Fail style" << styleFile.errorString();
+    }
+
     return a.exec();
 }
